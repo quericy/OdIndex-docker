89ffcb943978f16059e05acc68c24e4775982111
quericy <quericy@live.com>
2020-03-04 16:18:59 +0800

    modify():remove rewrite;

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -24,7 +24,7 @@ sed -i "s|'sitepath'=>''|'sitepath'=>'${SITE_PATH}'|" /var/www/html/index.php
 # custom nginx config
 if [ -f /etc/nginx/conf.d/nginx-custom.template.conf ];then
     cp -f /etc/nginx/conf.d/nginx-custom.template.conf /etc/nginx/conf.d/default.conf
-    sed -i "s|SITE_PATH|${SITE_PATH}|" /etc/nginx/conf.d/default.conf
+    #sed -i "s|SITE_PATH|${SITE_PATH}|" /etc/nginx/conf.d/default.conf
 fi
 
 sed -i "s|listen 80|listen ${PORT:-80}|" /etc/nginx/conf.d/nginx-custom.conf
